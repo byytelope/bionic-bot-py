ba50b1ea576c17813a62af6cec5ef44e08fd9b4d
Mohamed Shadhan <shadhanm@gmail.com>
2020-06-16 17:11:55 +0500

    GGwp

--- cogs/Music.py
+++ cogs/Music.py
@@ -11,6 +11,18 @@ class Music(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command(aliases=['j'])
+    async def join(self, ctx):
+        global voice
+        channel = ctx.message.author.voice.channel
+        voice = discord.utils.get(self.bot.voice_clients, guild=ctx.guild)
+
+        if voice and voice.is_connected():
+            await voice.move_to(channel)
+
+        else:
+            voice = await channel.connect()
+
     @commands.command(aliases=['l'])
     async def leave(self, ctx):
         voice = discord.utils.get(self.bot.voice_clients, guild=ctx.guild)
